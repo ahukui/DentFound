5fd07aef845cf93cf83f789dc8455384c8993168
ahukui <1548366814@qq.com>
2026-02-18 13:59:57 -0500

    Delete DentFound directory

--- DentFound/model/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from .language_model.llama import LlamaForCausalLM, Config
-    from .language_model.mpt import MptForCausalLM, MptConfig
-    from .language_model.mistral import MistralForCausalLM, MistralConfig
-except:
-    pass
